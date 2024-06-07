cbe65267154cf1dae1157d4b87958b1bef1a3fd0
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-06-07 17:11:04 +0200

    gg

--- src/app/api/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-import { LocalHostPath } from "../../functions/LocalHostPath";
-
-export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: LocalHostPath }),
-    tagTypes: ["Note", "User"],
-    endpoints: (builder) => ({}),
-});
