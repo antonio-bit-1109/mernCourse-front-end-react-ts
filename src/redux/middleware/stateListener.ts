6d59d30b5c9ee887facd67cd45eaadf0924c11df
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-06-20 23:56:14 +0200

    gg

--- src/redux/middleware/stateListener.ts
+++ src/redux/middleware/stateListener.ts
@@ -1,6 +1,6 @@
 // SCRIVO UN MIDDLEWARE LISTENER DI STATO CHE è IN ASCOLTO DELLA SCADENZA DEL TOKEN
 
-import { createListenerMiddleware, isAnyOf } from "@reduxjs/toolkit";
+import { ListenerEffectAPI, ThunkDispatch, UnknownAction, createListenerMiddleware, isAnyOf } from "@reduxjs/toolkit";
 import { TokenApi } from "../app/api/tokenApiSlice";
 import { jwtDecode } from "jwt-decode";
 import { RootState } from "../store";
@@ -8,6 +8,7 @@ import { IDecodedTokenStructure } from "../../interfaces/interfaces";
 import { UsersApi } from "../app/api/usersApiSlice";
 import { updateToken } from "../app/traditionalSlices/tokenReducer";
 import { NotesApi } from "../app/api/notesApiSlice";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
 
 const isTokenExpired = (token: string): boolean => {
     const decodedToken: IDecodedTokenStructure = jwtDecode(token);
@@ -21,7 +22,28 @@ const isTokenExpired = (token: string): boolean => {
 
 const listenerMiddleware = createListenerMiddleware();
 
-// Aggiungo un listener che viene attivato quando controllo se riesco a contattare gli endpoints dello user sul server.
+//tipizzo il parametro "action", utilizzato per risalire all'ultimo endpoint contattato, con tanto di informazioni utilizzate er contattare l'endpoint ( quindi i vari idUser, IdNote ecc ecc..)
+// export interface ReduxPayloadActionStructure {
+//     meta: {
+//         RTK_autoBatch: boolean;
+//         arg: {
+//             endpointName: string;
+//             // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//             fixedCacheKey: any;
+//             // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//             originalArgs: any;
+//         };
+//         track: boolean | undefined;
+//         type: string;
+//         requestId: string;
+//         requestStatus: string;
+//         startedTimeStamp: number;
+//     };
+//     payload: any;
+//     type: string;
+// }
+
+// questo listener viene attivato ogni volta che , con una fetch, tento di comunicare con uno di questi endpoint.
 listenerMiddleware.startListening({
     matcher: isAnyOf(
         UsersApi.endpoints.createNewUser.matchPending,
@@ -29,6 +51,7 @@ listenerMiddleware.startListening({
         UsersApi.endpoints.softDeleteUser.matchPending,
         UsersApi.endpoints.editUser.matchPending,
         UsersApi.endpoints.getAllUsers.matchPending,
+        UsersApi.endpoints.changeImageProfile.matchPending,
         NotesApi.endpoints.checkCompletedNote.matchPending,
         NotesApi.endpoints.createNewNote.matchPending,
         NotesApi.endpoints.deleteNote.matchPending,
@@ -36,15 +59,50 @@ listenerMiddleware.startListening({
         NotesApi.endpoints.getAllUserNotes.matchPending,
         NotesApi.endpoints.getSingleNote.matchPending
     ),
-    effect: async (_, listenerApi) => {
+    // quando tento di comunicare con l'endpoint, durante la fase di pending, se il token è scaduto, questo viene refreshato, contattando lendpoint per il refresh, e dispatchato nuovamente su redux.
+    effect: async (action, listenerApi) => {
         const state = listenerApi.getState() as RootState; // Tipo di stato
         const { accessToken } = state.token;
+
         if (accessToken && isTokenExpired(accessToken)) {
             try {
                 const response = await listenerApi.dispatch(TokenApi.endpoints.refresh.initiate({}));
                 const newAccessToken = await response.data.accessToken;
+
                 if (newAccessToken) {
                     listenerApi.dispatch(updateToken(newAccessToken));
+                    // // se ricavo un nuovo token dalla fetch lo salvo di nuovo du redux e rifaccio la fetch che ha fallito a causa del token
+                    // console.log(action);
+
+                    // // const PossibleApi = {
+                    // //     usersapi: UsersApi,
+                    // //     notesapi: NotesApi,
+                    // // };
+
+                    // const possibleEndpoints = {
+                    //     createNewUser: UsersApi.endpoints.createNewUser,
+                    //     getSingleUser: UsersApi.endpoints.getSingleUser,
+                    //     softDeleteUser: UsersApi.endpoints.softDeleteUser,
+                    //     editUser: UsersApi.endpoints.editUser,
+                    //     getAllUsers: UsersApi.endpoints.getAllUsers,
+                    //     changeImageProfile: UsersApi.endpoints.changeImageProfile,
+                    //     checkCompletedNote: NotesApi.endpoints.checkCompletedNote,
+                    //     createNewNote: NotesApi.endpoints.createNewNote,
+                    //     deleteNote: NotesApi.endpoints.deleteNote,
+                    //     editNote: NotesApi.endpoints.editNote,
+                    //     getAllUserNotes: NotesApi.endpoints.getAllUserNotes,
+                    //     getSingleNote: NotesApi.endpoints.getSingleNote,
+                    // };
+
+                    // const endpointName = action.meta.arg.endpointName;
+                    // const originalArgs = action.meta.arg.originalArgs;
+                    // // const apiSliceName = action.type.split("/")[0].toLowerCase();
+
+                    // if (possibleEndpoints[endpointName]) {
+                    //     await listenerApi.dispatch(possibleEndpoints[endpointName].initiate(originalArgs));
+                    // } else {
+                    //     console.error(`Endpoint ${endpointName} non trovato.`);
+                    // }
                 }
             } catch (err) {
                 console.error("Errore nel refresh del token", err);
@@ -52,5 +110,23 @@ listenerMiddleware.startListening({
         }
     },
 });
+
+// async function doTheFetchAgain(
+//     listenerApi: ListenerEffectAPI<unknown, ThunkDispatch<unknown, unknown, UnknownAction>, unknown>,
+//     possibleEndpoints: any,
+//     endpointName: string,
+//     args: any
+// ) {
+//     await listenerApi.dispatch(possibleEndpoints[endpointName].initiate(args));
+// }
+
+// async function doTheFetchAgain(apiSlice: any, endpointName: string, args: any, listenerApi: any) {
+//     const endpoint = apiSlice.endpoints[endpointName];
+//     if (endpoint) {
+//         await listenerApi.dispatch(endpoint.initiate(args));
+//     } else {
+//         console.error(`Endpoint ${endpointName} non trovato nello slice`);
+//     }
+// }
 
 export default listenerMiddleware;
